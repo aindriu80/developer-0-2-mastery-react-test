1a68482c6619df759a6c46ba26a48efc529143e8
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2020-06-11 17:11:25 +0100

    Initial Tests

--- script.test.js
+++ script.test.js
@@ -1,3 +1,19 @@
-it('This is a test', () => {
-    
+const googleSearch = require('./script');
+
+dbMock = [
+    'dog.com',
+    'chessepuff.com',
+    'disney.com',
+    'dogpictures.com'
+];
+
+it('This is a silly test', () => {
+    expect('hello').toBe('hello')
+    googleSearch('testtest', dbMock)
+})
+
+it('it is searching Google', () => {
+    expect(googleSearch('testtest', dbMock)).toEqual([])
+
+    expect(googleSearch('dog', dbMock)).toEqual(['dog.com','dogpictures.com'])
 })
