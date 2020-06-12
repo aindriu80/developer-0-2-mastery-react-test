39c4045bc186dd8a88d8dc9869fce7e981e92f9f
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2020-06-12 16:19:56 +0100

    Writing Tests

--- script.test.js
+++ script.test.js
@@ -7,6 +7,8 @@ dbMock = [
     'dogpictures.com'
 ];
 
+describe('googleSearch', () => {
+
 it('This is a silly test', () => {
     expect('hello').toBe('hello')
     googleSearch('testtest', dbMock)
@@ -16,4 +18,14 @@ it('it is searching Google', () => {
     expect(googleSearch('testtest', dbMock)).toEqual([])
 
     expect(googleSearch('dog', dbMock)).toEqual(['dog.com','dogpictures.com'])
+})
+
+it('work with undefined and null input', ()=>{
+    expect(googleSearch(undefined, dbMock)).toEqual([])
+    expect(googleSearch(null, dbMock)).toEqual([])
+})
+
+it('does not return more than 3 matches', ()=>{
+    expect(googleSearch('.com', dbMock).length).toEqual(3)
+})
 })
